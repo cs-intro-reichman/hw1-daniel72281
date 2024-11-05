747601cbf6cdb83a26cefcbe69d2550e57d0e16e
daniel daskal <daskal.daniel@gmail.com>
2024-11-05 13:35:35 +0200

    task completed

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,13 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		double bill = Double.parseDouble(args[3]) / 3.0;
+		bill = Math.ceil(bill);
+	    System.out.println("Dear "  + name1 + " " + name2 +" " +name3 + ": pay " + 
+				bill + " Shekels each.  ");
+		
+
 	}
 }
