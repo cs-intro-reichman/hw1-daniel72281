2f75fe95b71cd605c6d3bb33d5040dcc491d1c18
daniel daskal <daskal.daniel@gmail.com>
2024-11-12 15:15:41 +0200

    task corrected

--- TimeFormat.java
+++ TimeFormat.java
@@ -5,10 +5,23 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 				
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // created a set of "if" conditions to get the wanted out put
-		if (hours > 11 && minutes > 9)  {System.out.println(hours + ":" + minutes + " " + "PM");}
-		if (hours > 11 && minutes < 10) { System.out.println(hours + ":0" + minutes + " " + "PM");}
-		if (hours < 12 && minutes > 9) { System.out.println(hours + ":" + minutes + " " + "AM");}
-		if (hours < 12 && minutes < 10) {System.out.println(hours + ":0" + minutes + " " + "AM");}
+        // created a set of "if" conditions to get the wanted output, and ajusted hours to format
+		if (hours > 12 && minutes > 9)  {int a  = hours - 12;
+			 System.out.println(a + ":" + minutes + " " + "PM");}
+
+		if (hours > 12 && minutes < 10) {int a = hours - 12;
+			 System.out.println(a  + ":0" + minutes + " " + "PM");}
+
+		if (hours == 12 && minutes < 10)
+		 { System.out.println(hours + ":0" + minutes + " " + "PM");}
+
+		if (hours == 12 && minutes > 9)
+		 { System.out.println(hours + ":" + minutes + " " + "PM");}
+
+		if (hours < 12 && minutes > 9)
+		 { System.out.println(hours + ":" + minutes + " " + "AM");}
+
+		if (hours < 12 && minutes < 10)
+		 {System.out.println(hours + ":0" + minutes + " " + "AM");}
 	}
 }
