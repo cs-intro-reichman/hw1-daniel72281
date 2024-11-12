c1c5066bc48121de4a20e267ae0e4d0fa13358f8
daniel daskal <daskal.daniel@gmail.com>
2024-11-12 16:18:27 +0200

    task corrected

--- FVCalc.java
+++ FVCalc.java
@@ -8,7 +8,7 @@ public class FVCalc {
 		// uesd an equation to find the future value
 		double futureValue = currentValue * Math.pow(1+interestRate/100,years);
 		// i casted the args
-		System.out.println("after "  + ((int) years) + " years, a $" + ((int)currentValue) +" saved at " +
+		System.out.println("After "  + ((int) years) + " years, a $" + ((int)currentValue) +" saved at " +
 			interestRate + "%25 will yield $" + ((int) futureValue));
 	}
 }
